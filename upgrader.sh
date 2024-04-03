de64accb975a311561616f15f508662c37db5ae7
crouchr <richard.crouch@protonmail.com>
2024-04-03 21:02:08 +0100

    Aligned bash headers

--- upgrader.sh
+++ upgrader.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 #
 # (c) 2024 BlackRain Technologies
 #
+
 # Upgrader for VTRADR software
 # This script should only be used to upgrade an existing VTRADR installation
 
-#echo "Pull down latest version of VTRADR from GitHub"
-#cd ~
-#echo "Deleting of version of VTRADR code..."
-#rm -rf ~/vtradr
-#echo "Downloading latest version of VTRADR..."
-#git clone --depth 1 https://github.com/crouchr/vtradr.git
-#cd ~/vtradr
+# echo "Pull down latest version of VTRADR from GitHub"
+# cd ~
+# echo "Deleting of version of VTRADR code..."
+# rm -rf ~/vtradr
+# echo "Downloading latest version of VTRADR..."
+# git clone --depth 1 https://github.com/crouchr/vtradr.git
+# cd ~/vtradr
 
 # vagrant destroy --force
 # vagrant up
-
