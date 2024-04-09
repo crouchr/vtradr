df8bc9d540155c2baedb899e426a995f9085bab7
crouchr <richard.crouch@protonmail.com>
2024-04-09 23:11:49 +0100

    refactor

--- staging/setup-profile.sh
+++ staging/setup-profile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Entered setup-profile.sh..."
-# env
 
 function set_in_profile() {
   echo "$1" >> /home/vagrant/.bashrc
@@ -12,3 +11,5 @@ set_in_profile "export FMP_FREE_API_KEY=$FMP_FREE_API_KEY"
 set_in_profile "export PROTONMAIL_PLUS_BRIDGE_KEY=$PROTONMAIL_PLUS_BRIDGE_KEY"
 set_in_profile "export ALPHAVANTAGE_BASIC_API_KEY=$ALPHAVANTAGE_BASIC_API_KEY"
 set_in_profile "export ALPHAVANTAGE_FREE_API_KEY=$ALPHAVANTAGE_FREE_API_KEY"
+
+echo "Exited setup-profile.sh..."
