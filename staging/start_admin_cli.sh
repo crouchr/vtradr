0aecda72b380c17abedd95454c3e4245374cd5c7
crouchr <richard.crouch@protonmail.com>
2024-04-09 22:59:24 +0100

    refactor

---
+++ staging/start_admin_cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run from /home/vagrant to bring up tradr cli docker container
+docker pull registry:5000/tradr-cli
+docker run -v /var/lib/mysql/mysql.sock:/mysql.sock \
+--env "FMP_FREE_API_KEY=$FMP_FREE_API_KEY" \
+--env "VONAGE_API_SECRET=$VONAGE_API_SECRET" \
+--env "VONAGE_API_KEY=$VONAGE_API_KEY" \
+--env "PROTONMAIL_PLUS_BRIDGE_KEY=$PROTONMAIL_PLUS_BRIDGE_KEY" \
+--env "ALPHAVANTAGE_BASIC_API_KEY=$ALPHAVANTAGE_BASIC_API_KEY" \
+--env "ALPHAVANTAGE_FREE_API_KEY=$ALPHAVANTAGE_FREE_API_KEY" \
+--env "YAHOO_FREE_API_KEY=NONE_REQUIRED" \
+-it registry:5000/tradr-cli
