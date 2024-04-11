4e1c5dc0834fdb2d9349e581d7832d8c88e0ab7f
crouchr <richard.crouch@protonmail.com>
2024-04-11 07:32:45 +0100

    Run docker container in host mode so can access host services like MariaDB

--- staging/start_admin_cli.sh
+++ staging/start_admin_cli.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Run from /home/vagrant to bring up tradr cli docker container
 docker pull registry:5000/tradr-cli
-docker run -v /var/lib/mysql/mysql.sock:/mysql.sock \
+docker run --network host \
+-v /var/lib/mysql/mysql.sock:/mysql.sock \
 --env "FMP_FREE_API_KEY=$FMP_FREE_API_KEY" \
 --env "VONAGE_API_SECRET=$VONAGE_API_SECRET" \
 --env "VONAGE_API_KEY=$VONAGE_API_KEY" \
