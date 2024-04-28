c013f74139d293260a73c0980fb530f11e794005
crouchr <richard.crouch@protonmail.com>
2024-04-28 14:12:16 +0100

    Environment refactoring

--- common_scripts/start_stocks_scanner.sh
+++ common_scripts/start_stocks_scanner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run automatically from /home/vagrant to run stocks scanner (for troubleshooting) docker container
 docker pull registry:5000/tradr-scanner
-docker run --root --network host \
+docker run -u root --network host \
 -v /var/lib/mysql/mysql.sock:/mysql.sock \
 --env "FMP_FREE_API_KEY=$FMP_FREE_API_KEY" \
 --env "VONAGE_API_SECRET=$VONAGE_API_SECRET" \
