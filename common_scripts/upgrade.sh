f358b3ce89a1148634134de10575986be30e1c94
crouchr <richard.crouch@protonmail.com>
2024-05-02 13:23:47 +0100

    Added additional scripts for running daemons

--- common_scripts/upgrade.sh
+++ common_scripts/upgrade.sh
@@ -2,18 +2,18 @@
 # Pull latest TRADR docker images
 
 echo ' '
-echo "Installing latest TRADR CLI..."
-echo "------------------------------"
+echo "Installing latest TRADR CLI"
+echo "---------------------------"
 docker pull registry:5000/tradr-cli
 echo ' '
 
-echo "Installing TRADR Stock Scanner..."
-echo "---------------------------------"
+echo "Installing TRADR Stock Scanner"
+echo "------------------------------"
 docker pull registry:5000/tradr-scanner
 echo ' '
 
-echo "Installing latest TRADR Bitcoin Grabber..."
-echo "------------------------------------------"
+echo "Installing latest TRADR Bitcoin Grabber"
+echo "---------------------------------------"
 docker pull registry:5000/tradr-bitcoin-grabber
 echo ' '
 
