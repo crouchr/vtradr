73d13a7a4429750f0c2daadc8006f5b5fc420895
crouchr <richard.crouch@protonmail.com>
2024-05-02 13:06:45 +0100

    Added additional scripts for running daemons

--- common_scripts/set_symlinks.sh
+++ common_scripts/set_symlinks.sh
@@ -2,9 +2,9 @@
 
 echo "Entered set_symlinks.sh..."
 
-sudo mv /home/vagrant/configure.sh /usr/local/bin/configure.sh
-sudo chmod +x /usr/local/bin/configure.sh
-ln -sf /usr/local/bin/configure.sh /usr/local/bin/configure
+sudo mv /home/vagrant/cli.sh /usr/local/bin/cli.sh
+sudo chmod +x /usr/local/bin/cli.sh
+ln -sf /usr/local/bin/cli.sh /usr/local/bin/cli
 
 sudo mv /home/vagrant/upgrade.sh /usr/local/bin/upgrade.sh
 sudo chmod +x /usr/local/bin/upgrade.sh
