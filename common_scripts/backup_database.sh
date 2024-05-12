af1b503f108dd8e30deb55581208beb1dbf89893
crouchr <richard.crouch@protonmail.com>
2024-05-12 23:08:34 +0100

    bug : cosmetic change

--- common_scripts/backup_database.sh
+++ common_scripts/backup_database.sh
@@ -4,6 +4,7 @@
 logger 'entered backup_database.sh'
 
 echo "The best time to perform backup and restore is after 21:30 or before 08:30 UK time when markets are closed"
+
 echo "Backing up 'tradrdb' database to /home/vagrant/dbase_backups/tradrdb-backup.sql on this node..."
 mysqldump -u root -psecretsql tradrdb > /home/vagrant/dbase_backups/tradrdb-backup.sql
 echo "Backed up 'tradrdb' database to /home/vagrant/dbase_backups/tradrdb-backup.sql from this node"
