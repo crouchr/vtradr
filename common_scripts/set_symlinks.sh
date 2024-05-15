94dab92378f74f8ee1a4e2d8846cd66f4c775c2e
crouchr <richard.crouch@protonmail.com>
2024-05-15 10:45:29 +0100

    bug : added Commodities Grabber

--- common_scripts/set_symlinks.sh
+++ common_scripts/set_symlinks.sh
@@ -30,8 +30,8 @@ sudo mv /home/vagrant/version_tradr.sh /usr/local/bin/version_tradr.sh
 sudo chmod +x /usr/local/bin/version_tradr.sh
 ln -sf /usr/local/bin/version_tradr.sh /usr/local/bin/version
 
-sudo mv /home/vagrant/run_stock_scanner.sh /usr/local/bin/run_stock_scanner.s
-sudo chmod +x /usr/local/bin/run_stock_scanner.s
+sudo mv /home/vagrant/run_stock_scanner.sh /usr/local/bin/run_stock_scanner.sh
+sudo chmod +x /usr/local/bin/run_stock_scanner.sh
 ln -sf /usr/local/bin/run_stock_scanner.sh /usr/local/bin/run_stock_scanner
 
 echo "Exited set_symlinks.sh..."
