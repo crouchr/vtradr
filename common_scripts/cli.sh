93449ce9d2fdd26fb752d469f97a0b2c772364cf
crouchr <richard.crouch@protonmail.com>
2024-05-29 08:52:46 +0100

    Adding sound to Docker

--- common_scripts/cli.sh
+++ common_scripts/cli.sh
@@ -9,7 +9,7 @@ docker run -u root --quiet --privileged \
 --log-opt max-size=10m \
 --log-opt max-file=10 \
 -v /var/lib/mysql/mysql.sock:/mysql.sock \
--d /dev/snd:/dev/snd \
+--device /dev/snd:/dev/snd \
 --env "SQL_DB_HOSTNAME=$SQL_DB_HOSTNAME" \
 --env "FMP_FREE_API_KEY=$FMP_FREE_API_KEY" \
 --env "VONAGE_API_SECRET=$VONAGE_API_SECRET" \
