6ba6ae92fbfa3061b397f03aaaa31edf9f37dfe1
crouchr <richard.crouch@protonmail.com>
2024-06-04 07:58:30 +0100

    bug fix : set_symlinks

--- common_scripts/set_symlinks.sh
+++ common_scripts/set_symlinks.sh
@@ -14,9 +14,10 @@ sudo mv /home/vagrant/help_tradr.sh /usr/local/bin/help_tradr.sh
 sudo chmod +x /usr/local/bin/help_tradr.sh
 ln -sf /usr/local/bin/help_tradr.sh /usr/local/bin/help_tradr
 
-sudo mv /home/vagrant/run_tradr.sh /usr/local/bin/run_tradr.sh
-sudo chmod +x /usr/local/bin/run_tradr.sh
-ln -sf /usr/local/bin/run_tradr.sh /usr/local/bin/run_tradr
+# not needed
+# sudo mv /home/vagrant/run_tradr.sh /usr/local/bin/run_tradr.sh
+# sudo chmod +x /usr/local/bin/run_tradr.sh
+# ln -sf /usr/local/bin/run_tradr.sh /usr/local/bin/run_tradr
 
 sudo mv /home/vagrant/backup_database.sh /usr/local/bin/backup_database.sh
 sudo chmod +x /usr/local/bin/backup_database.sh
@@ -30,19 +31,9 @@ sudo mv /home/vagrant/version_tradr.sh /usr/local/bin/version_tradr.sh
 sudo chmod +x /usr/local/bin/version_tradr.sh
 ln -sf /usr/local/bin/version_tradr.sh /usr/local/bin/version
 
-# No longer needed
-# sudo mv /home/vagrant/run_stocks_scanner.sh /usr/local/bin/run_stocks_scanner.sh
-# sudo chmod +x /usr/local/bin/run_stocks_scanner.sh
-# ln -sf /usr/local/bin/run_stocks_scanner.sh /usr/local/bin/run_stocks_scanner
-
 # Run the stock grabbers
 sudo mv /home/vagrant/run_tradr_daemons.sh /usr/local/bin/run_tradr_daemons.sh
 sudo chmod +x /usr/local/bin/run_tradr_daemons.sh
 ln -sf /usr/local/bin/run_tradr_daemons.sh /usr/local/bin/run_tradr_daemons
-
-# Swing Trading scanner - no longer needed
-# sudo mv /home/vagrant/run_swing_trader.sh /usr/local/bin/run_swing_trader.sh
-# sudo chmod +x /usr/local/bin/run_swing_trader.sh
-# ln -sf /usr/local/bin/run_swing_trader.sh /usr/local/bin/run_swing_trader
 
 echo "Exited set_symlinks.sh..."
