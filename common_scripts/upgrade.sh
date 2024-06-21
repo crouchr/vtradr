2c0aa12a0a61793eadef66581518692fee8a75a4
crouchr <richard.crouch@protonmail.com>
2024-06-21 23:51:45 +0100

    Updated env vars

--- common_scripts/upgrade.sh
+++ common_scripts/upgrade.sh
@@ -10,50 +10,50 @@ echo "---------------------------"
 docker pull registry:5000/tradr-cli
 echo ' '
 
-echo "Installing TRADR Swing Trader Stock Scanner"
-echo "-------------------------------------------"
-docker pull registry:5000/tradr-scanner
-echo ' '
+# echo "Installing TRADR Swing Trader Stock Scanner"
+# echo "-------------------------------------------"
+# docker pull registry:5000/tradr-scanner
+# echo ' '
 
-echo "Installing latest TRADR Bitcoin Grabber"
-echo "---------------------------------------"
-docker pull registry:5000/tradr-bitcoin-grabber
-echo ' '
+# echo "Installing latest TRADR Bitcoin Grabber"
+# echo "---------------------------------------"
+# docker pull registry:5000/tradr-bitcoin-grabber
+# echo ' '
 
-echo "Installing latest TRADR Ibit Grabber"
-echo "------------------------------------"
-docker pull registry:5000/tradr-ibit-grabber
-echo ' '
+# echo "Installing latest TRADR Ibit Grabber"
+# echo "------------------------------------"
+# docker pull registry:5000/tradr-ibit-grabber
+# echo ' '
 
-echo "Installing latest TRADR Indices Grabber"
-echo "---------------------------------------"
-docker pull registry:5000/tradr-indices-grabber
-echo ' '
+# echo "Installing latest TRADR Indices Grabber"
+# echo "---------------------------------------"
+# docker pull registry:5000/tradr-indices-grabber
+# echo ' '
 
-echo "Installing latest TRADR Treasuries Grabber"
-echo "------------------------------------------"
-docker pull registry:5000/tradr-treasuries-grabber
-echo ' '
+# echo "Installing latest TRADR Treasuries Grabber"
+# echo "------------------------------------------"
+# docker pull registry:5000/tradr-treasuries-grabber
+# echo ' '
 
-echo "Installing latest TRADR Commodities Grabber"
-echo "-------------------------------------------"
-docker pull registry:5000/tradr-commodities-grabber
-echo ' '
+# echo "Installing latest TRADR Commodities Grabber"
+# echo "-------------------------------------------"
+# docker pull registry:5000/tradr-commodities-grabber
+# echo ' '
 
-echo "Installing latest TRADR Tepper Indicators"
-echo "-----------------------------------------"
-docker pull registry:5000/tradr-tepper-indicators
-echo ' '
+# echo "Installing latest TRADR Tepper Indicators"
+# echo "-----------------------------------------"
+# docker pull registry:5000/tradr-tepper-indicators
+# echo ' '
 
-echo "Installing latest TRADR Core Ratios"
-echo "-----------------------------------"
-docker pull registry:5000/tradr-core-ratios
-echo ' '
+# echo "Installing latest TRADR Core Ratios"
+# echo "-----------------------------------"
+# docker pull registry:5000/tradr-core-ratios
+# echo ' '
 
-echo "Installing latest TRADR Assets Tracker"
-echo "--------------------------------------"
-docker pull registry:5000/tradr-assets-tracker
-echo ' '
+# echo "Installing latest TRADR Assets Tracker"
+# echo "--------------------------------------"
+# docker pull registry:5000/tradr-assets-tracker
+# echo ' '
 
 echo "Installing latest TRADR Exchange Rate Service"
 echo "---------------------------------------------"
