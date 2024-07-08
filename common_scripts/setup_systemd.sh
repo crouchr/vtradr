dfe6005ccac45019a9ea7829cb48cac59acbef78
crouchr <richard.crouch100@gmail.com>
2024-07-08 08:41:13 +0100

    Added startup scripts for Candlestick Analyser

--- common_scripts/setup_systemd.sh
+++ common_scripts/setup_systemd.sh
@@ -5,4 +5,7 @@ echo "Entered set_systemd.sh..."
 systemctl enable exchange-rate-service.service
 systemctl start exchange-rate-service.service
 
+systemctl enable candlestick-analyser-service.service
+systemctl start candlestick-analyser-service.service
+
 echo "Exited set_systemd.sh..."
