d7238f2b270f7c6807f1965340923d910edaa77d
crouchr <richard.crouch100@gmail.com>
2024-11-27 09:49:58 +0000

    bug fix

--- common_scripts/change_vagrant_password.sh
+++ common_scripts/change_vagrant_password.sh
@@ -2,6 +2,6 @@
 # If connected to Internet do not want the default password set
 echo 'Changing default vagrant password...'
 
-echo "vagrant:FunkPrepper1968!" | sudo chpasswd
+echo "vagrant:FunkyPrepper1968!" | sudo chpasswd
 
 echo 'Changed default vagrant password.'
