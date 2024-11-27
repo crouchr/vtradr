9caec2c7cb33bdfb30df1c72e6d10c990412858b
crouchr <richard.crouch100@gmail.com>
2024-11-27 07:51:42 +0000

    vagrant password change

--- common_scripts/change_vagrant_password.sh
+++ common_scripts/change_vagrant_password.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # If connected to Internet do not want the default password set
+echo 'Changing default vagrant password...'
+
 echo "vagrant:FunkPrepper1968!" | sudo chpasswd
+
+echo 'Changed default vagrant password.'
