f55fbdf2a2b7c734fb53b0d1e10b685a53633092
Richard Crouch <richard.crouch@protonmail.com>
2024-11-28 13:07:05 +0000

    Added logging to firewall

--- common_scripts/configure_firewall.sh
+++ common_scripts/configure_firewall.sh
@@ -10,13 +10,13 @@ echo "Entered configure_firewall.sh..."
 #sudo systemctl enable firewalld
 
 # Allow all incoming traffic from 192.168.1.0/24
-sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix ="Access from LAN 192.168" source address="192.168.1.0/24" accept'
+sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from LAN 192.168" source address="192.168.1.0/24" accept'
 
 # Allow all incoming traffic from 172.31.0.0/16
-sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix ="Access from LAN 172.31" source address="172.31.0.0/16" accept'
+sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from LAN 172.31" source address="172.31.0.0/16" accept'
 
 # Allow SSH traffic from 86.169.0.0/16
-sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix ="SSHD from Adrian" source address="86.169.0.0/16" service name="ssh" accept'
+sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="SSHD from Adrian" source address="86.169.0.0/16" service name="ssh" accept'
 
 # Reload firewalld to apply the changes
 sudo firewall-cmd --reload
