ed02800968f77666af6fcb4456599aaf181dd03c
crouchr <richard.crouch100@gmail.com>
2024-11-29 07:09:04 +0000

    env_vars for users

--- common_scripts/cli.sh
+++ common_scripts/cli.sh
@@ -11,6 +11,7 @@ docker run -u root --quiet --privileged \
 -v /var/lib/mysql/mysql.sock:/mysql.sock \
 -v ./reports:/reports \
 -v ./yahoo_fin_historical_data:/yahoo_fin_historical_data \
+--env "TRADR_USERNAME=$USER" \
 --env "SQL_DB_HOSTNAME=$SQL_DB_HOSTNAME" \
 --env "FMP_FREE_API_KEY=$FMP_FREE_API_KEY" \
 --env "VONAGE_API_SECRET=$VONAGE_API_SECRET" \
