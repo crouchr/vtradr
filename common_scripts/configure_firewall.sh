949dbcab0d52394bdb1931c43e238f5f06f26281
Richard Crouch <richard.crouch@protonmail.com>
2024-12-02 07:53:43 +0000

    Added logging to firewall

--- common_scripts/configure_firewall.sh
+++ common_scripts/configure_firewall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # FIXME : this doesn't seem to be possible in the Packer run - to be investigated
-# Allow SSH from certain public ranges to cut down on the internet scanner noise
+# Allow SSH only from the LAN as remote access is via the Jumpbox (Ubuntu Lenovo) on 192.168.1.137
 
 echo "Entered configure_firewall.sh..."
 
@@ -9,14 +9,17 @@ echo "Entered configure_firewall.sh..."
 #sudo systemctl start firewalld
 #sudo systemctl enable firewalld
 
+# Allow SSH from Jumpbox 192.168.1.137
+sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from JUMPBOX: " source address="192.168.1.137" port port=22 protocol=tcp accept'
+
 # Allow all incoming traffic from 192.168.1.0/24
-sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from LAN 192.168" source address="192.168.1.0/24" accept'
+sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from LAN 192.168: " source address="192.168.1.0/24" accept'
 
-# Allow all incoming traffic from 172.31.0.0/16
-sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from LAN 172.31" source address="172.31.0.0/16" accept'
+# Allow all incoming traffic from 172.16.0.0/12 Monitor this to see if ANY 172.x.x.x access is needed to Tradr to run OK
+sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="Access from LAN 172.31: " source address="172.16.0.0/12" accept'
 
-# Allow SSH traffic from 86.169.0.0/16
-sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" log prefix="SSHD from Adrian" source address="86.169.0.0/16" service name="ssh" accept'
+# Explicitly drop ALL other traffic
+sudo firewall-cmd --permanent --add-rich-rule='rule family=ipv4 log prefix="FIREWALL-DROP: " level=info'
 
 # Reload firewalld to apply the changes
 sudo firewall-cmd --reload
@@ -24,7 +27,6 @@ sudo firewall-cmd --reload
 # Confirmation message
 echo "Firewall rules configured successfully:
 - Allowed all traffic from 192.168.1.0/24
-- Allowed all traffic from 172.31.0.0/16
-- Allowed SSH traffic from 86.169.0.0/16"
+- Allowed all traffic from 172.16.0.0/12"
 
 echo "Exited configure_firewall.sh"
