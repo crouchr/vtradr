649680090e840f622c487d747b9f241d581bdf40
Richard Crouch <richard.crouch@protonmail.com>
2025-02-15 12:28:05 +0000

    Added logs share and additional users

--- common_scripts/setup_env_vars.sh
+++ common_scripts/setup_env_vars.sh
@@ -6,8 +6,12 @@ echo "Entered setup_env_vars.sh to set ENV vars for users..."
 rm -rf /home/admin/.tradrrc
 rm -rf /home/johnnybench/.tradrrc
 rm -rf /home/sector/.tradrrc
+rm -rf /home/sector2/.tradrrc
 rm -rf /home/sniper/.tradrrc
 rm -rf /home/revesting/.tradrrc
+rm -rf /home/revesting2/.tradrrc
+rm -rf /home/revesting3/.tradrrc
+rm -rf /home/revesting4/.tradrrc
 rm -rf /home/shorts/.tradrrc
 rm -rf /home/bitcoin/.tradrrc
 rm -rf /home/watcher/.tradrrc
@@ -22,8 +26,12 @@ rm -rf /home/oscar/.tradrrc
 touch /home/admin/.tradrrc
 touch /home/johnnybench/.tradrrc
 touch /home/sector/.tradrrc
+touch /home/sector2/.tradrrc
 touch /home/sniper/.tradrrc
 touch /home/revesting/.tradrrc
+touch /home/revesting2/.tradrrc
+touch /home/revesting3/.tradrrc
+touch /home/revesting4/.tradrrc
 touch /home/shorts/.tradrrc
 touch /home/bitcoin/.tradrrc
 touch /home/watcher/.tradrrc
@@ -38,8 +46,12 @@ touch /home/oscar/.tradrrc
 chmod 777 /home/admin/.tradrrc
 chmod 777 /home/johnnybench/.tradrrc
 chmod 777 /home/sector/.tradrrc
+chmod 777 /home/sector2/.tradrrc
 chmod 777 /home/sniper/.tradrrc
 chmod 777 /home/revesting/.tradrrc
+chmod 777 /home/revesting2/.tradrrc
+chmod 777 /home/revesting3/.tradrrc
+chmod 777 /home/revesting4/.tradrrc
 chmod 777 /home/shorts/.tradrrc
 chmod 777 /home/bitcoin/.tradrrc
 chmod 777 /home/watcher/.tradrrc
@@ -56,8 +68,12 @@ function set_in_profile() {
   echo "$1" >> /home/admin/.tradrrc
   echo "$1" >> /home/johnnybench/.tradrrc
   echo "$1" >> /home/sector/.tradrrc
+  echo "$1" >> /home/sector2/.tradrrc
   echo "$1" >> /home/sniper/.tradrrc
   echo "$1" >> /home/revesting/.tradrrc
+  echo "$1" >> /home/revesting2/.tradrrc
+  echo "$1" >> /home/revesting3/.tradrrc
+  echo "$1" >> /home/revesting4/.tradrrc
   echo "$1" >> /home/shorts/.tradrrc
   echo "$1" >> /home/bitcoin/.tradrrc
   echo "$1" >> /home/watcher/.tradrrc
