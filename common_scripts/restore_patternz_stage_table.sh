ba5b4a8837c3ffc6159cfa1848a97721e3df56fd
Richard Crouch <richard.crouch@protonmail.com>
2025-03-22 13:20:30 +0000

    Bug Fix : Added backup-company-table and restore-company-table commands

--- common_scripts/restore_patternz_stage_table.sh
+++ common_scripts/restore_patternz_stage_table.sh
@@ -6,7 +6,7 @@ logger 'entered restore_patternz_stage_table.sh'
 echo "The best time to perform backup and restore is after 21:30 or before 08:30 UK time when markets are closed"
 
 echo "Restoring the 'tradrdb' database 'patternz_stage' table FROM /home/vagrant/dbase_backups/tradrdb-patternz-stage-backup.sql on this node..."
-mysql -u root -p secretsql tradrdb < /home/vagrant/dbase_backups/tradrdb-patternz-stage-backup.sql
+mysql -u root -psecretsql tradrdb < /home/vagrant/dbase_backups/tradrdb-patternz-stage-backup.sql
 echo "Restored the 'tradrdb' database 'patternz_stage' table FROM /home/vagrant/dbase_backups/tradrdb-patternz-stage-backup.sql on this node"
 
 logger 'exited restore_patternz_stage_table.sh'
