1324edf0f29fba23698b3011d9642ba50ce187d9
Richard Crouch <richard.crouch@protonmail.com>
2025-03-22 11:13:19 +0000

    Bug Fix : Added backup-company-table and restore-company-table commands

--- common_scripts/restore_company_table.sh
+++ common_scripts/restore_company_table.sh
@@ -6,7 +6,7 @@ logger 'entered restore_company_table.sh'
 echo "The best time to perform backup and restore is after 21:30 or before 08:30 UK time when markets are closed"
 
 echo "Restoring 'tradrdb' database 'company' table FROM /home/vagrant/dbase_backups/tradrdb-company-backup.sql on this node..."
-mysqldump -u root -psecretsql tradrdb company < /home/vagrant/dbase_backups/tradrdb-company-backup.sql
+mysql -u root -p secretsql tradrdb company < /home/vagrant/dbase_backups/tradrdb-company-backup.sql
 echo "Restoring 'tradrdb' database 'company' table FROM /home/vagrant/dbase_backups/tradrdb-company-backup.sql on this node"
 
 logger 'exited restore_company_table.sh'
